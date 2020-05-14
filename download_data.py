d756697ceb4953a1874f919241a83c5b626501e1
JLee <justynleepc@gmail.com>
2020-05-14 15:28:12 -0500

    123

--- download_data.py
+++ download_data.py
@@ -5,14 +5,15 @@ import requests
 os.chdir('data')
 
 os.remove('match_map_stats.csv')
-match_url = 'https://assets.blz-contentstack.com/v3/assets/blt321317473c90505c/blt6227c17dad8ae329/5e67cde78286c81bdae79015/match_map_stats.zip'
+match_url = 'https://assets.blz-contentstack.com/v3/assets/blt321317473c90505c/blt3c792ae164693de8/5e95026d297b4d1b5ff03d35/match_map_stats.zip'
 r = requests.get(match_url, allow_redirects=True)
 open('maps.zip', 'wb').write(r.content)
 with zipfile.ZipFile('maps.zip', 'r') as zip_ref:
     zip_ref.extractall()
 os.remove('maps.zip')
 
-player_url = 'https://assets.blz-contentstack.com/v3/assets/blt321317473c90505c/blt18e13a8c2e83d9d7/5e67cde7a9f0fb732c24cf48/phs_2020.zip'
+os.remove('phs_2020_1.csv')
+player_url = 'https://assets.blz-contentstack.com/v3/assets/blt321317473c90505c/blt45c4e7fb827663e5/5e9502ce297b4d1b5ff03d3b/phs_2020.zip'
 r = requests.get(player_url, allow_redirects=True)
 open('2020_player.zip', 'wb').write(r.content)
 with zipfile.ZipFile('2020_player.zip', 'r') as zip_ref:
